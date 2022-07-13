4053f2193b5834c7fdb2fb6f6e6f415c6d64cc2c
LuKeDSP <lucasfpilatti@gmail.com>
2022-07-13 15:40:15 -0300

    First commit

---
+++ JavaScript/menu.js
@@ -0,0 +1,9 @@
+function paginaMuller() {
+    window.document.getElementById('muller').onclick = window.location.href = 'mullerHome.html';
+}
+function paginaPilatti() {
+    window.document.getElementById('pilatti').onclick = window.location.href = 'pilattiHome.html';
+}
+
+window.document.getElementById('muller').onclick = paginaMuller;
+window.document.getElementById('pilatti').onclick = paginaPilatti;
